036720eebb5ab464d214a29d4d0fea92e1dcd596
LucasLei <33343199+LucasLei@users.noreply.github.com>
2017-11-06 22:39:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,57 +1,146 @@
-// $(document).ready(function() {
 $.fn.extend({
-        control: function(tab) {
-            if (tab.effect === undefined) {
-                tab.effect = false;
-            } else {
-                tab.effect = true;
-            }
-            tab.eventType = tab.eventType || "click"
-                // 点击前颜色
-            tab.liColor = tab.liColor || "#fff";
-            tab.bgColor = tab.bgColor || "#000";
-            //点击字体变颜色
-            tab.liClickColor = tab.liClickColor || "red"
-            tab.bgClickColor = tab.bgClickColor || "#000";
-            //点击前字体颜色
-            $(".tab li").css({
-                    color: tab.liColor,
-                    backgroundColor: tab.bgColor
-                })
-                //显示第一个颜色
-            $(".tab li").eq(0).css({
-                color: tab.liClickColor
-            })
-
-
-
-            if (tab.effect) {
-                $("div").hide().eq(0).show();
-                //点击按钮触发事件（渐隐渐现）
-                if (tab.eventType == "click") {
-                    $(".tab li").click(function() {
-                        $(".tab li").eq($(this).index()).addClass("cur").siblings().removeClass('cur');
-                        $("div").hide().eq($(this).index()).fadeIn();
-                    });
-                }
-                if (tab.eventType == "mouseover") {
-                    // 鼠标移上触发事件
-                    $(".tab li").mouseover(function() {
-                        $(".tab li").eq($(this).index()).addClass("cur").siblings().removeClass('cur');
-                        $("div").hide().eq($(this).index()).fadeIn();
-                    })
-                }
-            } else {
-                //滑动
-                // $(".tab li").click(function() {
-
-                //         $(".tab li").find("div").css({
-                //             marginTop: "-202px"
-                //         })
-                //     }
-            }
-
+    tabs: function(tab) {
+        //获取实参设置默认值
+        if (tab.effect === undefined) {
+            tab.effect = true; //默认渐隐渐现
+        } else {
+            tab.effect = false; //滑动
+        }
+        // 点击前颜色
+        tab.btnColor = tab.btnColor || "#fff"
+            //点击btn字体变颜色
+        tab.btnClickColor = tab.btnClickColor || "#000"
+            //点击或鼠标放上
+        if (tab.clickOrMouseover === undefined) {
+            tab.clickOrMouseover = true
+        } else {
+            tab.clickOrMouseover = false
         }
 
-    })
-    // });
+        //为当前调用方法元素添加class
+        this.addClass("btn");
+        //当前元素父级的（div）孩子
+        var con = this.parent().children("div").eq(0);
+        //btn点击前字体颜色
+        $(".btn li").css({
+                color: tab.btnColor
+            })
+            //显示第一个btn颜色
+        $(".btn li").eq(0).css({
+            color: tab.btnClickColor
+        })
+
+        if (tab.clickOrMouseover) {
+            if (tab.effect) {
+                //隐藏con里面的所有
+                con.children().hide();
+                //显示con里面的第一个
+                con.children().eq(0).show();
+                $(".btn li").click(function() {
+                    $(".btn li").css({
+                        color: tab.btnColor
+                    })
+                    $(this).css({
+                            color: tab.btnClickColor
+                        })
+                        //btn的index值付给n
+                    var n = $(this).index();
+                    con.children().hide();
+                    //显示当前的内容
+                    con.children().eq(n).fadeIn();
+                })
+            } else {
+                // 创建div
+                var con_div = $("<div></div>");
+                con_div.appendTo(con);
+                //所用内容div插入con_div
+                con.children().appendTo(con_div);
+                // con_div下面第一个div的宽度
+                var con_divChildWidth = con_div.children().eq(0).width()
+                    //最外层div宽度溢出隐藏
+                con.css({
+                        width: con_divChildWidth,
+                        overflow: "hidden"
+                    })
+                    //包涵内容的div宽度
+                con_div.css({
+                    width: con_divChildWidth * con_div.children().length
+                })
+                con_div.children().css({
+                    float: "left"
+                })
+                $(".btn li").click(function() {
+                    //btn 点击前字体颜色
+                    $(".btn li").css({
+                            color: tab.btnColor
+                        })
+                        //点击之后的btn字体颜色
+                    $(this).css({
+                            color: tab.btnClickColor
+                        })
+                        //btn的index值付给n
+                    var n = $(this).index();
+                    con_div.animate({
+                        marginLeft: -(n * con_divChildWidth)
+                    })
+                })
+            }
+        } else {
+            if (tab.effect) {
+                //隐藏con里面的所有
+                con.children().hide();
+                //显示con里面的第一个
+                con.children().eq(0).show();
+                $(".btn li").mouseover(function() {
+                    $(".btn li").css({
+                        color: tab.btnColor
+                    })
+                    $(this).css({
+                            color: tab.btnClickColor
+                        })
+                        //btn的index值付给n
+                    var n = $(this).index();
+                    con.children().hide();
+                    //显示当前的内容
+                    con.children().eq(n).fadeIn();
+                })
+            } else {
+                // 创建div
+                var con_div = $("<div></div>");
+                con_div.appendTo(con);
+                //所用内容div插入con_div
+                con.children().appendTo(con_div);
+                // con_div下面第一个div的宽度
+                var con_divChildWidth = con_div.children().eq(0).width()
+                    //最外层div宽度溢出隐藏
+                con.css({
+                        width: con_divChildWidth,
+                        overflow: "hidden"
+                    })
+                    //包涵内容的div宽度
+                con_div.css({
+                    width: con_divChildWidth * con_div.children().length
+                })
+                con_div.children().css({
+                    float: "left"
+                })
+                $(".btn li").mouseover(function() {
+                    //btn 点击前字体颜色
+                    $(".btn li").css({
+                            color: tab.btnColor
+                        })
+                        //点击之后的btn字体颜色
+                    $(this).css({
+                            color: tab.btnClickColor
+                        })
+                        //btn的index值付给n
+                    var n = $(this).index();
+                    con_div.animate({
+                        marginLeft: -(n * con_divChildWidth)
+                    })
+                })
+            }
+        }
+
+    }
+})
